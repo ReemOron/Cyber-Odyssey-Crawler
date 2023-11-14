544acbf0a300b096ade968055100c220d1f4b7d4
shaharblu <111304387+shaharblu@users.noreply.github.com>
2023-11-14 17:23:25 +0200

    depth implementation
    made code go through links in the given depth

--- main.py
+++ main.py
@@ -7,21 +7,23 @@ import yaml
 
 parser = argparse.ArgumentParser(description='Crawler')
 parser.add_argument('--url', type=str, help='URL to crawler')
+parser.add_argument('--depth', type=int, help='Depth of the crawler')
 arguments = parser.parse_args()
 
-links = set()
+links = [set(arguments.url)]
 
-web_handler = urllib.request.urlopen(url=arguments.url)
-HTML_code = web_handler.read().decode("utf8")
+for layer in range(arguments.depth):
+    links.append(set())
+    for link in links[layer]:
+        web_handler = urllib.request.urlopen(url=link)
+        HTML_code = web_handler.read().decode("utf8")
+        last_found = 0
+        while HTML_code.find('href="', last_found) != -1:
+            href_place = HTML_code.find('href="', last_found) + 6
+            link = ""
+            while HTML_code[href_place] != '"':
+                link += HTML_code[href_place]
+                href_place += 1
+            links[layer+1].add(link)
+            last_found = HTML_code.find('href="', last_found) + 7
 
-last_found = 0
-while HTML_code.find('href="', last_found) != -1:
-    href_place = HTML_code.find('href="', last_found) + 6
-    link = ""
-    while HTML_code[href_place] != '"':
-        link += HTML_code[href_place]
-        href_place += 1
-    links.add(link)
-    last_found = HTML_code.find('href="', last_found) + 7
-
-print(links)
